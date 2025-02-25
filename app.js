bf715ceb6350fc4e19e5d5584e9bc4a2bb5bf49f
Kely dos Santos Vieira <kely.vieira@atmo.local>
2025-02-25 17:19:10 -0300

    Atualizando

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ function sortear() {
   //vai recuperar o value que foi digitado dentro da caixa de texto
   //parseInt - avisar que vai chegar numero do tipo inteiro
 
+  if (quantidade > ate - de + 1) {
+    alert(
+      'Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!',
+    );
+    return;
+  }
+
   let sorteados = [];
   let numero;
 
@@ -13,14 +20,37 @@ function sortear() {
 
     while (sorteados.includes(numero)) {
       numero = obterNumeroAleatorio(de, ate);
+      alert('Tentando obter número inédito');
     }
+
     sorteados.push(numero);
   }
 
   let resultado = document.getElementById('resultado');
   resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+  alterarStatusBotao();
 }
 
 function obterNumeroAleatorio(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao() {
+  let botao = document.getElementById('btn-reiniciar');
+  if (botao.classList.contains('container__botao-desabilitado')) {
+    botao.classList.remove('container__botao-desabilitado');
+    botao.classList.add('container__botao');
+  } else {
+    botao.classList.remove('container__botao');
+    botao.classList.add('container__botao-desabilitado');
+  }
+}
+
+function reiniciar() {
+  document.getElementById('quantidade').value;
+  document.getElementById('de').value;
+  document.getElementById('ate').value;
+  document.getElementById('resultado').innerHTML =
+    '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
+  alterarStatusBotao();
 }
